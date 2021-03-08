fcd1e9d3e24bee47e9892f1c909849cc20f34374
rondayvoo <rondayvoo911@gmail.com>
2021-03-08 22:47:51 +0800

    Initialization

---
+++ SoundDisplay.srcs/sources_1/new/Top_Student.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////////////
+//
+//  FILL IN THE FOLLOWING INFORMATION:
+//
+//  LAB SESSION DAY (Delete where applicable): MONDAY P.M, TUESDAY P.M, WEDNESDAY P.M, THURSDAY A.M., THURSDAY P.M
+//
+//  STUDENT A NAME: 
+//  STUDENT A MATRICULATION NUMBER: 
+//
+//  STUDENT B NAME: 
+//  STUDENT B MATRICULATION NUMBER: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Top_Student (
+    input  J_MIC3_Pin3,   // Connect from this signal to Audio_Capture.v
+    output J_MIC3_Pin1,   // Connect to this signal from Audio_Capture.v
+    output J_MIC3_Pin4    // Connect to this signal from Audio_Capture.v
+    // Delete this comment and include other inputs and outputs here
+    );
+
+    // Delete this comment and write your codes and instantiations here
+
+endmodule
